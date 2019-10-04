d59946abb18800bb1dcb14c5fa24da3e9e32f5bc
Roberto Piccini <roberto.piccini@gmail.com>
2019-10-04 16:19:07 +0200

    add feature to tickets from incoming emails

--- init.rb
+++ init.rb
@@ -1,8 +1,13 @@
 require 'redmine_switch_issue_status/issues_controller_patch'
+require 'redmine_switch_issue_status/mail_handler_patch'
 
 Rails.configuration.to_prepare do
   unless IssuesController.included_modules.include? RedmineSwitchIssueStatus::IssuesControllerPatch
     IssuesController.prepend(RedmineSwitchIssueStatus::IssuesControllerPatch)
+  end
+
+  unless MailHandler.included_modules.include? RedmineSwitchIssueStatus::MailHandlerPatch
+    MailHandler.prepend(RedmineSwitchIssueStatus::MailHandlerPatch)
   end
 end
 
@@ -12,7 +17,7 @@ Redmine::Plugin.register :redmine_switch_issue_status do
   description <<-eos
     automatically switch status of the issue from a starting status to another status when updating the description or adding a comment
   eos
-  version '1.0.2'
+  version '1.1.0'
   url 'https://github.com/piccio/redmine_switch_issue_status'
   author_url 'https://github.com/piccio'
 
