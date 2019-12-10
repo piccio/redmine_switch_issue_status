3d8697a1af8ee70cdba6b2be3b58761ddd1f21e3
Roberto Piccini <roberto.piccini@gmail.com>
2019-12-10 12:46:10 +0100

    fix the description

--- init.rb
+++ init.rb
@@ -10,9 +10,9 @@ Redmine::Plugin.register :redmine_switch_issue_status do
   name 'Redmine Switch Issue Status plugin'
   author 'Roberto Piccini'
   description <<-eos
-    automatically switch status of the issue from a starting status to another status when updating the description or adding a comment
+    automatically switch status of the issue from a starting status to another status when the assignee updates the description or adds a comment
   eos
-  version '2.0.0'
+  version '2.0.1'
   url 'https://github.com/piccio/redmine_switch_issue_status'
   author_url 'https://github.com/piccio'
 
